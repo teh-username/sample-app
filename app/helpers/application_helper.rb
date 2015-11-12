100901d5691fd2c3b938424fc6a8afa2fb4fae24
LA Roberto <louise.roberto.1337@gmail.com>
2015-11-12 18:42:52 +0800

    Add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Returns correct title
+  def full_title(page_title = '')
+    title = ""
+    title << " | #{page_title}" if !page_title.empty?
+    title = "Corgi" + title;
+  end
 end
