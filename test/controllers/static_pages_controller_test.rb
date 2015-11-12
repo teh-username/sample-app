783646f1021356983c36ed515ba5041e35ed8e49
LA Roberto <louise.roberto.1337@gmail.com>
2015-11-12 15:38:35 +0800

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Corgi"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Corgi | Home"
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Corgi | Help"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do 
     get :about
     assert_response :success
-    assert_select "title", "Corgi | About"
+    assert_select "title", "#{@base_title} | About"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "#{@base_title} | Contact"
   end
 end
